0a66a2cc9cfbd4963dfded7ed85a2b3df4c0569c
Luis Yoshikawa <luisfelipeyoshi@gmail.com>
2023-10-02 11:30:17 -0300

    changing the cards and the cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -11,23 +11,26 @@ const Card = () => {
     setConfirm(true)
   }
 
-  const pageContent = confirm ? <h2>Thank you for the order</h2> : <> <h2 className="offscreen">Cart</h2>
-  <ul className='cart'>{cart.map(item => {
-    return (
-      <CartLineItem key={item.sku} item={item} dispatch={dispatch} REDUCER_ACTIONS={REDUCER_ACTIONS} />
-    )
-  })}</ul>
-  <div className="cart__totals">
-    <p>Total Items: {totalItems}</p>
-    <p>Total Price: {totalPrice}</p>
-    <button className="cart__submit" disabled={!totalItems} onClick={onSubmitOrder}>
-      Place Order
-    </button>
-  </div>
+  const pageContent = confirm ? <h2 style={{margin: "16px"}}>Thank you for the order</h2> : <>   
+    <div  className="main main__cart">
+      <h2 className="offscreen">Cart</h2>
+      <ul className='cart'>{cart.map(item => {
+        return (
+          <CartLineItem key={item.sku} item={item} dispatch={dispatch} REDUCER_ACTIONS={REDUCER_ACTIONS} />
+        )
+      })}</ul>
+    </div>
+    <div className="cart__totals">
+      <p>Total Items: {totalItems}</p>
+      <p>Total Price: {totalPrice}</p>
+      <button className="cart__submit" disabled={!totalItems} onClick={onSubmitOrder}>
+        Place Order
+      </button>
+    </div>
   </>
 
   const content = (
-    <main className="main main__cart">
+    <main>
       {pageContent}
     </main>
   )
